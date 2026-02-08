e926bae88e5a984e4d157ff8cfc954d449b3f6ef
Hasaan Mehmood <hasaan.mehmood.9@gmail.com>
2026-02-08 23:52:28 +0800

    Initial Commit

---
+++ time_delta.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import datetime
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    from datetime import datetime
+
+    format_str = '%a %d %b %Y %H:%M:%S %z'
+
+    dt1 = datetime.strptime(t1, format_str)
+    dt2 = datetime.strptime(t2, format_str)
+
+    delta = abs(int((dt1 - dt2).total_seconds()))
+
+    return str(delta)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
